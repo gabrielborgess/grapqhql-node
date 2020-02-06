f9e353cca5c425a8593cd632109f5dc2230e5fff
Gabriel <gbordesc@uft.edu>
2020-02-06 13:08:53 -0300

    Adelantado

--- index.js
+++ index.js
@@ -3,13 +3,44 @@ const app=express();
 const express_graphql=require('express-graphql');
 const {buildSchema}=require('graphql');
 
+//DATOS
+const {libros}=require('./data.json')
+console.log(libros)
 const schema =buildSchema(`
 type Query{
-    message: String
+    libro(id:Int!):Libro
+    libros(tena:String):[Libro]
 }
+
+type Libro{
+    id:Int
+    titulo:String
+    autor:String
+    tema:String
+    url:String
+}
+
 `);
+
+let obtenerLibro=(args)=>{
+    let id=args.id;
+    return libros.filter(libro=>{
+        return libro.id==id;
+    })[0]
+}
+
+let obtenerLibros=(args)=>{
+    if (args.tema){
+        let tema=args.tema;
+        return libros.filter(libro=>libros.tema==tema)
+    }else{
+        return libros;
+    }
+}
+
 const root={
-    message:()=>"Prueba"
+    libro:obtenerLibro,
+    libros:obtenerLibros
 }
 app.use('/graphql',express_graphql({
     schema:schema,
